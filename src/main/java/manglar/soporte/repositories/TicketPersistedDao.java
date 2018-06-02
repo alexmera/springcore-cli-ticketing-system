69035e37aa220e7c34c6a9bec250e9e504ddf877
Alex Mera <alxmera@gmail.com>
2018-06-02 11:21:33 -0500

    Add persisted dao, backed by MapDB

--- src/main/java/manglar/soporte/repositories/TicketPersistedDao.java
+++ src/main/java/manglar/soporte/repositories/TicketPersistedDao.java
@@ -1,10 +1,13 @@
 package manglar.soporte.repositories;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import manglar.soporte.model.Ticket;
 import manglar.soporte.model.value.TicketValue;
 
@@ -21,37 +24,60 @@ public class TicketPersistedDao implements TicketDao {
 
   @Override
   public Ticket create(Ticket ticket) {
-    return null;
+    String json = ticketToJson(ticket);
+    persistenceMap.put(ticket.getId(), json);
+    return ticket;
   }
 
   @Override
   public Ticket getOne(long id) {
-    return null;
+    String json = persistenceMap.get(id);
+    if (json != null) {
+      return jsonToTicket(json);
+    }
+    throw new NoSuchElementException();
   }
 
   @Override
   public Optional<Ticket> findOne(long id) {
+    String json = persistenceMap.get(id);
+    if (json != null) {
+      return Optional.of(jsonToTicket(json));
+    }
     return Optional.empty();
   }
 
   @Override
   public List<Ticket> find(Predicate<Ticket> predicate) {
-    return null;
+    List<Ticket> tickets = new ArrayList<>();
+    persistenceMap.forEach((k, v) -> {
+      Ticket ticket = jsonToTicket(v);
+      if (predicate.test(ticket)) {
+        tickets.add(ticket);
+      }
+    });
+    return tickets;
   }
 
   @Override
   public List<Ticket> list() {
-    return null;
+    return persistenceMap.values()
+        .stream()
+        .map(this::jsonToTicket)
+        .collect(Collectors.toList());
   }
 
   @Override
   public Ticket update(Ticket ticket) {
-    return null;
+    persistenceMap.put(ticket.getId(), ticketToJson(ticket));
+    return ticket;
   }
 
   @Override
   public Ticket delete(long id) {
-    return null;
+    Ticket ticket = getOne(id);
+    persistenceMap.remove(id);
+    return ticket;
   }
 
   private String ticketToJson(Ticket ticket) {
